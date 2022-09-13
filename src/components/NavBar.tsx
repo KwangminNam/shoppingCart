197c9d21ddf758eaf8f378decadae97e7d754578
kwangminNam <johnpower@naver.com>
2022-09-13 22:43:10 +0900

    ADD dummy data , type

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <NavbarBs className='bg-white shadow-sm mb-3'>
+    <NavbarBs sticky='top' className='bg-white shadow-sm mb-3'>
       <Container>
         <Nav className='me-auto'>
           <Nav.Link to ="/" as={NavLink}>
@@ -17,7 +17,14 @@ const NavBar = () => {
             about
           </Nav.Link>
         </Nav>
-        <Button>ShoppingCart</Button>
+        <Button
+          style={{position:"relative"}}
+        >ShoppingCart
+          <div className="rounded-circle bg-danger d-flex justify-content-center align-items-center" style={{
+            color:"white",width:"1.5rem",height:"1.5rem",
+            position:"absolute",bottom:0,right:0,transform:"translate(25%,-25%)"
+            }}>3</div>
+        </Button>
       </Container>
     </NavbarBs>
   );
